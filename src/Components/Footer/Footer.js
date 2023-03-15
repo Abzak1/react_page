a67f3b9485bfc03c344c889cc70df7737ec74169
Abzak <abzak45@gmail.com>
2023-03-15 16:05:50 +0200

    /r

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -7,7 +7,7 @@ const Footer = () => {
 
   <div className="text-center p-3" >
    
-    <h6 className="text-white " >Baraka Multiservices Ltd  © 2023  All Rights Reserved.</h6>
+    <h6 className="text-white ">Baraka Multiservices Ltd  © 2023  All Rights Reserved.</h6>
   </div>
 
 </footer>
